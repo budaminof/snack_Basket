85d6342d0395ac1f03259acced217d70c94f4f1c
bud aminof <budaminof@gmail.com>
2016-04-11 11:38:06 -0600

    npm install cookie-session, knex, pg

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
