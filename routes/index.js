fed12e7df03515778880f4bdc1df0bd7cd793ecd
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-04-11 16:18:12 -0600

    Add item listings on main page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gnosh' });
-});
 
+router.get('/', function(req, res, next) {
+  knex.select('name','description','price','image_url').from('items')
+  .then(function(items) {
+    console.log(items)
+    res.render('index', {items });
+  })
+});
 
 module.exports = router;
