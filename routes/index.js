7ae9c66f76bf0a334457e83e958948982122ea38
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-04-12 13:03:19 -0600

    users get confirmation email when they signup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
+var fs = require('fs');
+var Handlebars = require("handlebars");
+
 var dotenv = require('dotenv');
 var sendgrid = require('sendgrid')('MatieuB', 'tenbusch7');
 
@@ -19,18 +22,30 @@ router.get('/', function(req, res, next) {
   })
 });
 
+//get file
+var regEmail = fs.readFileSync('./views/email.hbs','utf-8');
+
+//compile template
+var compiledTemplate = Handlebars.compile(regEmail);
+
 //sendgrid email test
 router.get('/email', function(req, res) {
+    console.log(compiledTemplate);
     sendgrid.send({
         to: ['bouchard.matthewj@gmail.com'],
         from: 'noreply@gnosh.com',
-        subject: 'hello world!',
-        text: 'It worked, got the basic emaily functioning! :D'
+        subject: 'Welcome from GNOSH.com',
+        html: compiledTemplate({firstName:'Matthew, Bud and & Daniel!!!'})
     }, function(err, json) {
         if(err) {
           res.send('oh no!!');
       } res.send('success!!!!!!!',json);
   })
 });
+//test render rich html email
+router.get('/preview', function(req,res){
+    res.render('email',{firstName:'Bud'})
+
+})
 
 module.exports = router;
