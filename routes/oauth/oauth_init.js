17097ef80175ce5fb5f013efd3f431fd48b12a96
bud aminof <budaminof@gmail.com>
2016-04-12 21:44:41 -0600

    local strategy!

--- routes/oauth/oauth_init.js
+++ routes/oauth/oauth_init.js
@@ -1,11 +1,11 @@
 var knex = require('knex')(require('../../knexfile')[process.env.DB_ENV]);
 
-module.exports.findOrCreate= function(profile, cb) {
+module.exports.findOrCreate= function(profile, cb2) {
     knex('users')
     .where({email: JSON.parse(profile._raw).emails[0].value})
     .then(function (user){
         if (user.length > 0){
-            cb();
+            cb2(null, user);
         }
         else {
             knex('users')
@@ -13,11 +13,12 @@ module.exports.findOrCreate= function(profile, cb) {
                 first_name: profile._json.name.givenName,
                 last_name: profile._json.name.familyName,
                 email: JSON.parse(profile._raw).emails[0].value,
-                oauth_type: 'google'
+                oauth_type: 'google',
+                image_url: profile._json.image.url
             })
             .returning('*')
             .then(function (user){
-                cb();
+                cb2(null, user);
             })
         }
     })
