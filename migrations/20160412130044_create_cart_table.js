6df9656b332151cb1c0d7375d7507293891a6516
bud aminof <budaminof@gmail.com>
2016-04-14 18:15:42 -0600

    fixed migration

--- migrations/20160412130044_create_cart_table.js
+++ migrations/20160412130044_create_cart_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
         table.increments();
         table.integer('user_id').references('users.id').onUpdate('CASCADE').onDelete('CASCADE');
         table.integer('item_id').references('items.id').onUpdate('CASCADE').onDelete('CASCADE');
-        table.boolean('paid').defaultTo('false').onUpdate('CASCADE').onDelete('CASCADE');
+        table.boolean('paid').defaultTo('false');
     })
 
 };
